e16469a1827724d99b195a667ae944dfacc97fd3
Farrah Shiela Urmeneta <farrah.s.urmeneta@hotmail.ca>
2018-10-22 23:41:40 -0600

    incorporated parsing of input of the 'of the' form

--- hierarchysearch.py
+++ hierarchysearch.py
@@ -4,6 +4,11 @@ date: 10.22.18
 
 checks off desired body parts/organs based on user input
 also removes the check based on user input
+
+to-be-added:
+    ability to stack parent specifics
+    eg.
+        superior lobe of the right lung of the lungs
 """
 
 
@@ -13,20 +18,20 @@ class Body(object):
         self.root = root
 
 
-    def select(self, bodyPart):
-        self.root.select(bodyPart)
+    def select(self, body_part, new_root=None):
+        self.root.select(body_part, new_root)
 
 
-    def deselect(self, bodyPart):
-        self.root.deselect(bodyPart)
+    def deselect(self, body_part, new_root=None):
+        self.root.deselect(body_part, new_root)
+
+    
+    def has_part(self, body_part, new_root=None):
+        return self.root.is_part(body_part, new_root)
 
 
     def print_state(self):
         self.root.print_state(0)
-
-    
-    def has_part(self, bodyPart):
-        return self.root.is_part(bodyPart)
 
 
 class BodyPart(object):
@@ -41,41 +46,49 @@ class BodyPart(object):
             self.children = children
 
 
-    def add_internal(self, *bodyParts):
+    def __repr__(self):
+        return self.name
+
+
+    def add_child(self, *body_parts):
         """ dynamic method to add one child or multiple children """
-        for i in range(len(bodyParts)):
-            self.children.append(bodyParts[i])
+        for i in range(len(body_parts)):
+            self.children.append(body_parts[i])
 
-
-    def get_part(self, bodyPart):
-        """ checks if a body part exists given a string representing the 
-        name of the body part. It is not case-sensitive
-        """
-        # if the body part in question is itself, then the body part
-        # exists
-        if bodyPart == self.name.lower():
+    
+    def _find_root(self, root):
+        """ locates a part and returns it """
+        if self.name.lower() == root:
             return self
-
-        # otherwise, we check its children to see if they are the
-        # body part we seek
+        elif self.children == []:
+            return None
+        
         for part in self.children:
-            # if one of the children are this part, then the body part
-            # exists
-            if part.is_part(bodyPart):
-                return part
-
-        # if we cannot find it, then it doesn't exist - perhaps
-        # not in this branch
-        return None
+            if part._find_root(root) is not None:
+                return part._find_root(root)
 
 
-    def is_part(self, bodyPart):
+    def is_part(self, body_part, root=None):
+        """ uses an internal method to check if the part is a child
+        it can specify which part (if there are ones with repeating names)
+        by specifying the desired root
+        """
+        # if there is a specified root, then we look for the body part there
+        if root is not None:
+            inner_root = self._find_root(root)
+            return inner_root._inner_is_part(body_part)
+        # else, we don't restrict the search and look everywhere
+        else:
+            return self._inner_is_part(body_part)
+
+
+    def _inner_is_part(self, body_part):
         """ checks if a body part exists given a string representing the 
         name of the body part. It is not case-sensitive
         """
         # if the body part in question is itself, then the body part
         # exists
-        if bodyPart == self.name.lower():
+        if body_part == self.name.lower():
             return True
 
         # otherwise, we check its children to see if they are the
@@ -83,7 +96,7 @@ class BodyPart(object):
         for part in self.children:
             # if one of the children are this part, then the body part
             # exists
-            if part.is_part(bodyPart):
+            if part.is_part(body_part):
                 return True
 
         # if we cannot find it, then it doesn't exist - perhaps
@@ -91,26 +104,63 @@ class BodyPart(object):
         return False
 
 
-    def select(self, bodyPart):
+    def select(self, body_part, root=None):
+        """ uses an internal method to select a part and its ancestors,
+        it can specify which part (if there are ones with repeating names)
+        by specifying the desired root
+        """
+        # if we are looking for a specific body part, we search
+        # where the root is
+        if root is not None:
+            inner_root = self._find_root(root)
+            if inner_root.is_part(body_part):
+                # we restrict the ancestor to the root for the particular
+                # body part
+                inner_root._inner_select(body_part)
+                # then we select all of the ancestors up to that root
+                self._inner_select(root)
+        # otherwise we select everything with that body part name
+        else:
+            self._inner_select(body_part)
+
+
+    def _inner_select(self, body_part):
         """ selects the desired part, as well as all of its ancestors """
         # if this is the part, then we select it
-        if bodyPart == self.name.lower():
+        if body_part == self.name.lower():
             self.selected = True
 
         # it may not be the case that this is the desired part, so we then
         # look through its children to select that
         for part in self.children:
             # if its child has been selected, then it is also selected
-            if (part.select(bodyPart) == True):
+            if (part._inner_select(body_part) == True):
                 self.selected = True
 
         return self.selected
 
 
-    def deselect(self, bodyPart):
+    def deselect(self, body_part, root=None):
+        """ uses an internal method to deselect a part and its children,
+        it can specify which part (if there are ones with repeating names)
+        by specifying the desired root
+        """
+        # if we are looking for a specific body part, we search
+        # where the root is
+        if root is not None:
+            inner_root = self._find_root(root)
+            # we restrict the ancestor to the root for the particular
+            # body part
+            inner_root._inner_deselect(body_part)
+        # otherwise we deselect everything with that body part name
+        else:
+            self._inner_deselect(body_part)
+        
+
+    def _inner_deselect(self, body_part):
         """ deselects the desired part, as well as all of its children """
         # if this is the body part, then it deselects itself
-        if self.name.lower() == bodyPart:
+        if body_part == self.name.lower():
             self.selected = False
             # as well as its children
             for part in self.children:
@@ -119,7 +169,7 @@ class BodyPart(object):
             # otherwise, it keeps searching through its children to
             # deselect the desired body part
             for part in self.children:
-                part.deselect(bodyPart)
+                part.deselect(body_part)
 
 
     def print_state(self, level):
@@ -162,11 +212,11 @@ def intiaialize_body():
 
     # setting up parts within heart
     heart = BodyPart("Heart")
-    heart.add_internal(BodyPart("Left Ventricle"))
-    heart.add_internal(BodyPart("Right Ventricle"))
-    heart.add_internal(BodyPart("Left Aorta"))
-    heart.add_internal(BodyPart("Right Aorta"))
-    heart.add_internal(BodyPart("Septum"))
+    heart.add_child(BodyPart("Left Ventricle"))
+    heart.add_child(BodyPart("Right Ventricle"))
+    heart.add_child(BodyPart("Left Aorta"))
+    heart.add_child(BodyPart("Right Aorta"))
+    heart.add_child(BodyPart("Septum"))
 
     # preparing chest class
     chest = BodyPart("Chest", [lungs, heart])
@@ -184,13 +234,13 @@ def parse_user_input(user_input):
 
 
 if __name__ == '__main__':
-    
-    body = intiaialize_body()
 
     QUIT_CMDS = ["q", "quit", "exit"]
     SELECT_CMDS = ["s", "select"]
     DESELECT_CMDS = ["d", "deselect"]
 
+    body = intiaialize_body()
+    
     while True:
         print ("\nThis is the current layout: \n")
         body.print_state()
@@ -215,40 +265,22 @@ if __name__ == '__main__':
 
             print("\nPlease type your desired part.\n")
             print("In the case that you would like to choose a part that")
-            print("stems from a particular part, then enter:") 
-            print("<part name> of the <parent part>; ex.") 
-            print("the Superior Lobe of the Right Lung\n")
-            print("This method can be stacked, ex.")
-            print("Superior Lobe of the Right Lung of the Lungs\n")
-
-            print("WARNING: CURRENTLY, YOU CAN ONLY SELECT AND DESELECT")
-            print("ONE BODY PART AT A TIME. ATTEMPTING TO USE THE")
-            print("'OF THE' FORM WILL RESULT IN UNEXPECTED BEHAVIORS\n")
+            print("stems from a particular parent, then enter:\n") 
+            print("\t<part name> of the <parent part>")
+            print("\tex. the Superior Lobe of the Right Lung\n")
 
             body_part_input = input("> ").lower()
-
-            ## currently unfinished code for "of the" format
-            '''
-            body_part_input = parse_user_input(body_part_input)
+            parts = parse_user_input(body_part_input)
             
-            parts_exist = True
-
-            for part in body_part_input:
-                if not body.has_part(part):
-                    print("That body part does not exist")
-                    parts_exist = False
-                    break
-                    
-            if parts_exist:
-                action(body_part_input)
-            '''
-
-            if body.has_part(body_part_input):
-                action(body_part_input)
+            if len(parts) > 1 and body.has_part(parts[0], parts[1]):
+                action(parts[0], parts[1])
+            elif len(parts) == 1 and body.has_part(parts[0]):
+                action(parts[0])
             else:
-                print("That body part does not exist")
+                print("\nThat body part does not exist!")
+                
+                
 
         else:
             print("Please choose a valid command.")
-            continue
 
